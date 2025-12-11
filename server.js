74b6a8a4a036f46caf435b479a14b4a8281b80b7
ifauzeee <ifauze343@gmail.com>
2025-12-12 03:43:14 +0700

    feat: Implement initial public pages for index, login, and admin, along with server routing and assets.

--- server.js
+++ server.js
@@ -340,6 +340,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
 app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
